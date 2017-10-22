6de7433ef1b56ee837e6b04d113824fe409dae52
Kevin Godell <kevin.godell@gmail.com>
2017-10-21 21:01:27 -0500

    revert for testing

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function Pipe2Pam() {
     }
     //set objectMode to true so that we can pipe objects instead of just strings
     Transform.call(this, {objectMode: true});
+    //needed if pam image byte length is larger than chunk size(mac 8192, unbuntu 65535, windows ~ 93000+)
+    this._buffer = Buffer.allocUnsafe(0);
     //parsing first chunk should be looking for image header info
     this._parseChunk = this._findHeaders;
 }
@@ -48,15 +50,8 @@ Pipe2Pam.prototype._findHeaders = function (chunk) {
                     this._loi = this._loh + this._lop;
                     //eoi position
                     this._eoi = this._soi + this._loi;
-                    //determine which pam parser to use based on size of pam relative to size of chunk
-                    this._chunksNeeded = Math.ceil(this._loi / length);
-                    if (this._chunksNeeded > 1) {
-                        this._chunksCounter = 0;
-                        this._chunks = [];
-                        this._parseChunk = this._findBigPam;//pam image spans across more than 1 chunk
-                    } else {
-                        this._parseChunk = this._findSmallPam;//pam image(s) exist in only 1 chunk
-                    }
+                    //only needed to find first header, now only parse for whole pam image
+                    this._parseChunk = this._findPam;
                     //start parsing this chunk for pam image
                     this._parseChunk(chunk);
                     break;
@@ -67,43 +62,34 @@ Pipe2Pam.prototype._findHeaders = function (chunk) {
     }
 };
 
-//pam considered big because its file size exceeds 1 piped chunk
-Pipe2Pam.prototype._findBigPam = function (chunk) {
-    this._chunks[this._chunksCounter++] = chunk;
-    if (this._chunksCounter === this._chunksNeeded) {
-        const pam = Buffer.concat(this._chunks, this._loi);
-        const data = {pam: pam, headers: pam.slice(0, this._loh), pixels: pam.slice(this._loh), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype, chunks: this._chunksCounter};
-        if (this._readableState.pipesCount > 0) {
-            this.push(data);
-        }
-        if (this.listenerCount('pam') > 0) {
-            this.emit('pam', data);
-        }
-        this._chunksCounter = 0;
-        this._chunks.length = 0;
-    }
-};
-
-//pam considered small because it fits within 1 piped chunk, chunk may be packed with a single pam or multiple pams
-Pipe2Pam.prototype._findSmallPam = function (chunk) {
-    const length = chunk.length;
-    if (length === this._loi) {
-        const data = {pam: chunk, headers: chunk.slice(0, this._loh), pixels: chunk.slice(this._loh), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype, chunks: 1};
-        if (this._readableState.pipesCount > 0) {
-            this.push(data);
-        }
-        if (this.listenerCount('pam') > 0) {
-            this.emit('pam', data);
-        }
-    } else {//assumed to be less because of calculation of image size vs chunk size done in findHeaders()
-        const pamsPerChunk = length / this._loi;
-        for (let i = 0, soi = 0, sop = this._loh, eoi = this._loi; i < pamsPerChunk; i++, soi += this._loi, sop += this._loi, eoi += this._loi) {
-            const data = {pam: chunk.slice(soi, eoi), headers: chunk.slice(soi, sop), pixels: chunk.slice(sop, eoi), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype, chunks: 1};
+//find complete pam image and pass along to pipe and emit event
+Pipe2Pam.prototype._findPam = function (chunk) {
+    this._buffer = Buffer.concat([this._buffer, chunk]);
+    const bufferLength = this._buffer.length;
+    while (true) {
+        if (bufferLength < this._eoi) {
+            if (this._soi > 0) {
+                this._buffer = this._buffer.slice(this._soi);
+            }
+            this._soi = 0;
+            this._eoi = this._loi;
+            break;
+        } else {
+            let data = {pam: this._buffer.slice(this._soi, this._eoi), headers: this._buffer.slice(this._soi, this._soi + this._loh), pixels: this._buffer.slice(this._soi + this._loh, this._eoi), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype};
             if (this._readableState.pipesCount > 0) {
                 this.push(data);
             }
             if (this.listenerCount('pam') > 0) {
                 this.emit('pam', data);
+            }
+            if (bufferLength === this._eoi) {
+                this._buffer = Buffer.allocUnsafe(0);
+                this._soi = 0;
+                this._eoi = this._loi;
+                break;
+            } else {
+                this._soi = this._eoi;
+                this._eoi = this._soi + this._loi;
             }
         }
     }
@@ -117,16 +103,16 @@ Pipe2Pam.prototype._transform = function (chunk, encoding, callback) {
 
 //reset some values
 Pipe2Pam.prototype._flush = function (callback) {
+    this._buffer = Buffer.allocUnsafe(0);
     delete this._headers;
     delete this._soi;
     delete this._loh;
     delete this._lop;
     delete this._loi;
     delete this._eoi;
-    delete this._chunksCounter;
-    delete this._chunks;
     this._parseChunk = this._findHeaders;
     callback();
 };
 
 module.exports = Pipe2Pam;
+//todo: to avoid multiple if-else statements, in findHeader function, change chunk parser based on whether receiving single image per chunk, multiple images per chunk, or single image spread across multiple chunks
