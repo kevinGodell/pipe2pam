cadcbf79557bd0277f863b546d10b558088a8100
Kevin Godell <kevin.godell@gmail.com>
2017-10-23 17:03:22 -0500

    added new test

---
+++ tests/test4.js
@@ -0,0 +1,70 @@
+// jshint esversion: 6, globalstrict: true, strict: true
+'use strict';
+
+const fps = 100;
+
+const scale = 1/2;
+
+console.time(`=====> fps=${fps} scale=${scale} single pam split between multiple piped chunks`);
+
+const assert = require('assert');
+
+const P2P = require('../index');
+
+const spawn = require('child_process').spawn;
+
+const pamCount = 1000;
+
+let pamCounter = 0;
+
+const params = [
+    /* log info to console */
+    '-loglevel',
+    'quiet',
+    '-stats',
+
+    /* use an artificial video input */
+    '-re',
+    '-f',
+    'lavfi',
+    '-i',
+    'testsrc=size=1920x1080:rate=15',
+
+    /* set output flags */
+    '-an',
+    '-c:v',
+    'pam',
+    '-pix_fmt',
+    'rgb24',
+    '-f',
+    'image2pipe',
+    '-vf',
+    `fps=${fps},scale=iw*${scale}:ih*${scale}`,
+    '-frames',
+    pamCount,
+    'pipe:1'
+];
+
+const p2p = new P2P();
+
+p2p.on('pam', (data) => {
+    pamCounter++;
+    assert(data.width * data.height * data.depth === data.pixels.length, 'Pixels are not the correct length');
+    assert(data.headers.length + data.pixels.length === data.pam.length, 'Headers plus pixels are not the correct length');
+    const pam = data.pam;
+    assert(pam[0] === 0x50 && pam[1] === 0x37 && pam[2] === 0x0A, 'Start of pam is not correct');
+});
+
+const ffmpeg = spawn('ffmpeg', params, {stdio: ['ignore', 'pipe', 'inherit']});
+
+ffmpeg.on('error', (error) => {
+    console.log(error);
+});
+
+ffmpeg.on('exit', (code, signal) => {
+    assert(code === 0, `FFMPEG exited with code ${code} and signal ${signal}`);
+    assert(pamCounter === pamCount, `did not get ${pamCount} pams`);
+    console.timeEnd(`=====> fps=${fps} scale=${scale} single pam split between multiple piped chunks`);
+});
+
+ffmpeg.stdout.pipe(p2p);
