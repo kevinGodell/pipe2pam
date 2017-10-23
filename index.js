6f0ebcd40d174fd4a1b6156792dd6ad767c0614b
Kevin Godell <kevin.godell@gmail.com>
2017-10-23 17:05:33 -0500

    minimize call to Buffer.concat which was killing performance on mac because of limited piped chunk size of 8192

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ function Pipe2Pam() {
     }
     //set objectMode to true so that we can pipe objects instead of just strings
     Transform.call(this, {objectMode: true});
-    //needed if pam image byte length is larger than chunk size(mac 8192, unbuntu 65535, windows ~ 93000+)
-    this._buffer = Buffer.allocUnsafe(0);
     //parsing first chunk should be looking for image header info
     this._parseChunk = this._findHeaders;
 }
@@ -33,9 +31,9 @@ Pipe2Pam.prototype._parseHeaders = function (data) {
 //find pam headers and reassign this._parseChunk to Pipe2Pam.prototype._findPam
 Pipe2Pam.prototype._findHeaders = function (chunk) {
     for (let i = 0, length = chunk.length; i < length; i++) {
-        //P7\n = 0x50 0x37 0x0A
+        //find start of image P7\n = 0x50 0x37 0x0A
         if (chunk[i] === 0x50 && chunk[i + 1] === 0x37 && chunk[i + 2] === 0x0A) {
-            this._soi = i;
+            this._soi = i;//might return if this._soi !== 0
             i += 56;
             for (i; i < length; i++) {
                 //DR\n = 0x44 0x52 0x0A
@@ -52,6 +50,12 @@ Pipe2Pam.prototype._findHeaders = function (chunk) {
                     this._eoi = this._soi + this._loi;
                     //only needed to find first header, now only parse for whole pam image
                     this._parseChunk = this._findPam;
+                    //push chunks into array until we have enough to make a complete pam image
+                    this._chunks = [];
+                    //length of chunks array
+                    this._chunksLength = 0;
+                    // total length of bytes in chunks array
+                    this._chunksTotalLength = 0;
                     //start parsing this chunk for pam image
                     this._parseChunk(chunk);
                     break;
@@ -62,35 +66,43 @@ Pipe2Pam.prototype._findHeaders = function (chunk) {
     }
 };
 
-//find complete pam image and pass along to pipe and emit event
 Pipe2Pam.prototype._findPam = function (chunk) {
-    this._buffer = Buffer.concat([this._buffer, chunk]);
-    const bufferLength = this._buffer.length;
-    while (true) {
-        if (bufferLength < this._eoi) {
-            if (this._soi > 0) {
-                this._buffer = this._buffer.slice(this._soi);
-            }
-            this._soi = 0;
-            this._eoi = this._loi;
-            break;
-        } else {
-            let data = {pam: this._buffer.slice(this._soi, this._eoi), headers: this._buffer.slice(this._soi, this._soi + this._loh), pixels: this._buffer.slice(this._soi + this._loh, this._eoi), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype};
+    const chunkLength = chunk.length;
+    this._chunks.push(chunk);
+    this._chunksLength++;
+    this._chunksTotalLength += chunkLength;
+    if (this._loi === this._chunksTotalLength) {
+        const buffer = Buffer.concat(this._chunks, this._loi);
+        const data = {pam: buffer, headers: buffer.slice(0, this._loh), pixels: buffer.slice(this._loh), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype, chunks: this._chunksLength};
+        if (this._readableState.pipesCount > 0) {
+            this.push(data);
+        }
+        if (this.listenerCount('pam') > 0) {
+            this.emit('pam', data);
+        }
+        this._chunks = [];
+        this._chunksLength = 0;
+        this._chunksTotalLength = 0;
+    } else if (this._loi < this._chunksTotalLength) {
+        const divided = Math.floor(this._chunksTotalLength / this._loi);
+        const remainder = this._chunksTotalLength % this._loi;
+        const buffer = Buffer.concat(this._chunks, this._chunksTotalLength);
+        for (let i = 0, soi = 0, sop = this._loh, eoi = this._loi; i < divided; i++, soi += this._loi, sop += this._loi, eoi += this._loi) {
+            const data = {pam: buffer.slice(soi, eoi), headers: buffer.slice(soi, sop), pixels: buffer.slice(sop, eoi), width: this._headers.width, height: this._headers.height, depth: this._headers.depth, maxval: this._headers.maxval, tupltype: this._headers.tupltype, chunks: this._chunksLength};
             if (this._readableState.pipesCount > 0) {
                 this.push(data);
             }
             if (this.listenerCount('pam') > 0) {
                 this.emit('pam', data);
             }
-            if (bufferLength === this._eoi) {
-                this._buffer = Buffer.allocUnsafe(0);
-                this._soi = 0;
-                this._eoi = this._loi;
-                break;
-            } else {
-                this._soi = this._eoi;
-                this._eoi = this._soi + this._loi;
-            }
+        }
+        this._chunks = [];
+        this._chunksLength = 0;
+        this._chunksTotalLength = 0;
+        if (remainder !== 0) {
+            this._chunks.push(chunk.slice(chunkLength - remainder));
+            this._chunksLength++;
+            this._chunksTotalLength += remainder;
         }
     }
 };
@@ -103,16 +115,17 @@ Pipe2Pam.prototype._transform = function (chunk, encoding, callback) {
 
 //reset some values
 Pipe2Pam.prototype._flush = function (callback) {
-    this._buffer = Buffer.allocUnsafe(0);
     delete this._headers;
     delete this._soi;
     delete this._loh;
     delete this._lop;
     delete this._loi;
     delete this._eoi;
+    delete this._chunks;
+    delete this._chunksLength;
+    delete this._chunksTotalLength;
     this._parseChunk = this._findHeaders;
     callback();
 };
 
 module.exports = Pipe2Pam;
-//todo: to avoid multiple if-else statements, in findHeader function, change chunk parser based on whether receiving single image per chunk, multiple images per chunk, or single image spread across multiple chunks
